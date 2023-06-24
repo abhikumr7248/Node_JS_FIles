d6721eab5bc6e43103be4d92138a67c364fdecdc
abhishek <abhikumar7248225833@gmail.com>
2023-06-24 08:11:53 +0530

    create server

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const http = require('http')
+// const http = require('http')
 
+// http.createServer((req, resp) => {
+//     resp.write(" < h2 > this is abhishek< /h2>");
+//     resp.end();
+// }).listen(4500);
+
+//console.warn("Try Abhi, bhumi");
+
+const http = require('http');
 http.createServer((req, resp) => {
-    resp.write(" < h2 > this is abhishek< /h2>");
+    resp.writeHead(200, { 'Content-Type': 'application\json' });
+    resp.write(JSON.stringify({ name: 'Abhishek Kumar', email: 'abhikumar7248225833@gmail.com' }));
     resp.end();
-}).listen(4500);
+}).listen(5000);
