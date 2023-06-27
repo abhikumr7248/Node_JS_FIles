fce50adb7582a97367821ead4d43e30c6f0a5fed
abhishek <abhikumar7248225833@gmail.com>
2023-06-27 10:25:53 +0530

    api with static data

---
+++ data.js
@@ -0,0 +1,7 @@
+const d = [
+    { name: 'Abhishek Kumar', email: 'abhikumar7248225833,gmail.com' },
+    { name: 'Bhumika Chauhan', email: 'chauhanbhumika572@gmail.com' },
+    { name: 'Sanjeev Kumar', email: 'abhikumar5683@gmail.com' },
+    { name: 'Madan Singh', email: 'dk459306@gmail.com' }
+]
+module.exports = d;
--- index.js
+++ index.js
@@ -8,8 +8,11 @@
 //console.warn("Try Abhi, bhumi");
 
 const http = require('http');
+const d = require('./data');
 http.createServer((req, resp) => {
     resp.writeHead(200, { 'Content-Type': 'application\json' });
-    resp.write(JSON.stringify({ name: 'Abhishek Kumar', email: 'abhikumar7248225833@gmail.com' }));
+    // resp.write(JSON.stringify({ name: 'Abhishek Kumar', email: 'abhikumar7248225833@gmail.com' }));
+    resp.write(JSON.stringify(d));
+
     resp.end();
 }).listen(5000);
